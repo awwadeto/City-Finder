26f5fdd35bba75f0efb1a5d0c53c92a2a268d4b5
Mohammad Awwad <msa492@gmail.com>
2019-08-03 09:54:08 +0300

    Initial Commit

---
+++ City FinderTests/City_FinderTests.swift
@@ -0,0 +1,34 @@
+//
+//  City_FinderTests.swift
+//  City FinderTests
+//
+//  Created by Awwad on 8/3/19.
+//  Copyright © 2019 awwad. All rights reserved.
+//
+
+import XCTest
+@testable import City_Finder
+
+class City_FinderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
